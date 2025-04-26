7cc38af980a3726291c24df449a19af131288870
SurendharSSenthil <surendharsenthil257@gmail.com>
2025-04-26 16:35:49 +0530

    Employees crud using prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
